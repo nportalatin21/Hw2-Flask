786e589b3865622cb4164c0a6a2480a778aa9028
Nicole Portalatin <nportalatin@xpanxion.com>
2023-07-06 20:50:50 -0400

    Update

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 
-function index() {
+function Index() {
+    const [message, setMessage] = useState("Loading")
+    const [poeple, setPeople] = useState([])
+
+  useEffect(() => {
+
+    fetch("http://localhost:8080/api/home")
+    .then((response) => response.json())
+    .then((data) => {
+        // message = 'Loading'
+        //Once data is retrieved
+        //message = data.message
+
+        setMessage(data.message);
+        setPeople(data.people);
+        console.log(data.people)
+
+    })
+},[])
+
   return (
-    <div>index</div>
+
+    <div>
+    <div>{message}</div>
+    <div>{
+      poeple.map((person,index) => (
+        <div key={index}>{person}</div>
+      ))}
+      </div>
+    </div>
   )
 }
 
-export default index
+export default Index
--- server/venv/server.py
+++ server/venv/server.py
@@ -2,14 +2,18 @@ from flask import Flask, jsonify
 
 # app insntance
 app = Flask(__name__)
+CORS(app)
 
+
+# /api/home
 @app.route("/api/home", methods=['GET'])
 def return_home():
     return jsonify({
-        'message': "Hello World!"
+        'message': "Like this video if this helped!"
+        'people': ['Jack', 'Harry', 'Apran']
 
 
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=8080)
